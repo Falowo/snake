e3d4e40a8f4ef6dd431f7474d3a3958b855125dd
josselin krikorian <obarayego@gmail.com>
2021-03-11 19:00:38 +0100

    déplacements du snake

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './style.css';
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const direction = 'e';
 const gridElem = 40; // 20 elem * 20
 const snake = [
     [9, 9],
@@ -27,15 +28,37 @@ const drawApple = () => {
     ctx.fillRect(apple[0] * gridElem, apple[1] * gridElem, gridElem, gridElem);
 };
 
-drawMap();
-drawSnake();
-drawApple();
+const updateSnakePosition = () => {
+    let head;
+    switch (direction) {
+        case 'e':
+            head = [snake[0][0] + 1, snake[0][1]];
+            break;
+        case 'o':
+            head = [snake[0][0] - 1, snake[0][1]];
+            break;
+        case 'n':
+            head = [snake[0][0], snake[0][1] - 1];
+            break;
+        case 's':
+            head = [snake[0][0], snake[0][1] + 1];
+            break;
+
+        default:
+            break;
+    }
+    snake.unshift(head);
+    snake.pop();
+};
 
 const move = () => {
+    updateSnakePosition();
     drawMap();
     drawSnake();
     drawApple();
-    // requestAnimationFrame(move);
+    setTimeout(() => {
+        requestAnimationFrame(move);
+    }, 1000);
 };
 
 requestAnimationFrame(move);
