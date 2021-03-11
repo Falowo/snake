791e4e57ebf624c432e14135a362db2904abf285
josselin krikorian <obarayego@gmail.com>
2021-03-11 20:42:42 +0100

    changement de direction et gameover

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import './style.css';
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const direction = 'e';
+let speed = 100;
+let direction = 'e';
 const gridElem = 40; // 20 elem * 20
 const snake = [
     [9, 9],
@@ -28,6 +29,56 @@ const drawApple = () => {
     ctx.fillRect(apple[0] * gridElem, apple[1] * gridElem, gridElem, gridElem);
 };
 
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case "ArrowRight":
+            direction = "e";
+            break;
+        case "ArrowLeft":
+            direction = "o";
+            break;
+        case "ArrowUp":
+            direction = "n";
+            break;
+        case "ArrowDown":
+            direction = "s";
+            break;
+
+        default:
+            break;
+    }
+});
+
+const addSpeed = () => {
+
+    const interval = setInterval(() => {
+        speed += 10;
+        console.log(speed);
+    }, 1000);
+
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 85000);
+};
+
+const gameover = () => {
+    if (snake[0][0] > 19 ||
+        snake[0][0] < 0 ||
+        snake[0][1] > 19 ||
+        snake[0][1] < 0
+    ) {
+        return true;
+    }else {
+        const[head, ...body] = snake;
+        for(let bodyElem of body) {
+            if (bodyElem[0] === head[0] && bodyElem[1] === head[1]){
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
 const updateSnakePosition = () => {
     let head;
     switch (direction) {
@@ -49,16 +100,22 @@ const updateSnakePosition = () => {
     }
     snake.unshift(head);
     snake.pop();
+    return gameover();
 };
 
 const move = () => {
-    updateSnakePosition();
-    drawMap();
-    drawSnake();
-    drawApple();
-    setTimeout(() => {
-        requestAnimationFrame(move);
-    }, 1000);
+    if (!updateSnakePosition()) {
+        drawMap();
+        drawSnake();
+        drawApple();
+        setTimeout(() => {
+            requestAnimationFrame(move);
+        }, 1000 - speed);
+    } else {
+        alert('Perdu');
+    }
 };
 
 requestAnimationFrame(move);
+
+addSpeed();
